1863bc678d5d2ea5a4a75c8509aa7f0da87afe68
Marc Castillo <marccastillo621@gmail.com>
2024-12-08 16:42:33 +0800

    optimized design for q&a and announcement in bigger screens

--- src/pages/QuestionsAnswers.jsx
+++ src/pages/QuestionsAnswers.jsx
@@ -6,12 +6,12 @@ const QuestionAndAnswers = (props) => {
     <>
       <div className="md:flex">
         <Sidebar activeTab="QA" />
-        <div className="p-4 w-screen flex flex-col h-screen md:p-8 xl:p-32">
+        <div className="p-4 w-screen flex flex-col h-screen md:p-8 xl:p-20">
           <h2 className="text-2xl font-bold lg:text-4xl">Q&A Section</h2>
           <div className="grid grid-cols-3 gap-8 h-full">
             {/* left side div in desktop */}
-            <div className="col-span-3 xl:col-span-2 xl:">
-              <button className="w-full p-2 bg-blue-nav mt-4 rounded-full text-white">
+            <div className="flex flex-col items-center col-span-3 xl:col-span-2">
+              <button className="w-full p-2 bg-blue-nav mt-4 rounded-full text-white shadow-md  lg:mb-4">
                 Ask a Question
               </button>
               <div className="question">
@@ -46,7 +46,7 @@ const QuestionAndAnswers = (props) => {
             {/* right side div in desktop */}
             <div className="hidden p-4 rounded-xl h-full xl:block xl:col-span-1 xl:bg-blue-white ">
               <h2>Announcements</h2>
-              <div className="question">
+              <div className="question w-full">
                 {" "}
                 <div
                   className="flex gap-6 mb-4"
