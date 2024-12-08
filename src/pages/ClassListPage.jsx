e3af7e06817aaed21c3bd6182c5646cd8a317881
Marc Castillo <marccastillo621@gmail.com>
2024-12-08 18:57:38 +0800

    optimized the class list for all screen size

--- src/pages/ClassListPage.jsx
+++ src/pages/ClassListPage.jsx
@@ -13,11 +13,14 @@ const ClassList = (props) => {
           <h2 className="text-2xl font-bold lg:text-4xl">Classlist</h2>
           <div className="h-[calc(100vh-7rem)] w-full mt-4 p-4 ">
             {/* grid for the header list of students */}
-            <div className="grid grid-cols-6 gap-4 mb-4 pb-4 pl-2 border-b-2 border-black/50">
-              <h2 className="col-span-3">Name</h2>
-              <h2 className="col-span-2">Classroom Role</h2>
-              <h2 className="col-span-1 pl-6">Status</h2>
+            <div className="text-xs grid grid-cols-6 gap-4 mb-4 pb-4 px-2 border-b-2 border-black/50">
+              <h2 className="col-span-3 md:col-span-3">Name</h2>
+              <h2 className="col-span-3 place-self-end md:col-span-2 md:place-self-start">
+                Classroom Role
+              </h2>
+              <h2 className="hidden md:block md:col-span-1 pl-2">Status</h2>
             </div>
+            {/* lists of students */}
             <div className="p-2">
               <div
                 className="grid grid-cols-6 gap-4 w-full border-b-2 border-black/20 py-4  cursor-pointer"
@@ -25,39 +28,45 @@ const ClassList = (props) => {
                   navigate("/user/Marc Lowel J. Castillo");
                 }}
               >
-                <p className="col-span-3">Marc Lowel J. Castillo</p>
-                <p className="col-span-2 bg-purple-200 p-2 text-center rounded-full w-36 text-xs">
+                <p className="col-span-3 md:col-span-3">
+                  Marc Lowel J. Castillo
+                </p>
+                <p className="col-span-3 place-self-end bg-purple-200 p-2 text-center rounded-full w-36 text-xs  md:col-span-2 md:place-self-start">
                   CWTS Beadle
                 </p>
-                <p className="col-span-1 bg-green-200 p-2 text-center rounded-full max-w-36 text-xs">
+                <p className="hidden md:block md:col-span-1 bg-green-200 p-2 text-center rounded-full max-w-36 text-xs">
                   Regular
                 </p>
               </div>
               <div
-                className="grid grid-cols-6 gap-4 w-full  border-b-2 border-black/20 py-4  cursor-pointer"
+                className="grid grid-cols-6 gap-4 w-full border-b-2 border-black/20 py-4  cursor-pointer"
                 onClick={() => {
-                  navigate("/user/Ydan Ganosa");
+                  navigate("/user/Marc Lowel J. Castillo");
                 }}
               >
-                <p className="col-span-3">Ydan Ganosa</p>
-                <p className="col-span-2 bg-amber-200 p-2 text-center rounded-full w-36 text-xs">
-                  Admin/Creatives
+                <p className="col-span-3 md:col-span-3">
+                  Marc Lowel J. Castillo
                 </p>
-                <p className="col-span-1 bg-blue-200 p-2 text-center rounded-full max-w-36 text-xs">
+                <p className="col-span-3 place-self-end bg-purple-200 p-2 text-center rounded-full w-36 text-xs  md:col-span-2 md:place-self-start">
+                  Committee
+                </p>
+                <p className="hidden md:block md:col-span-1 bg-blue-200 p-2 text-center rounded-full max-w-36 text-xs">
                   Irregular
                 </p>
               </div>
               <div
-                className="grid grid-cols-6 gap-4 w-full  border-b-2 border-black/20 py-4 cursor-pointer"
+                className="grid grid-cols-6 gap-4 w-full border-b-2 border-black/20 py-4  cursor-pointer"
                 onClick={() => {
-                  navigate("/user/Raj Bruel");
+                  navigate("/user/Marc Lowel J. Castillo");
                 }}
               >
-                <p className="col-span-3">Raj Bruel</p>
-                <p className="col-span-2 bg-slate-200 p-2 text-center rounded-full w-36 text-xs">
+                <p className="col-span-3 md:col-span-3">
+                  Marc Lowel J. Castillo
+                </p>
+                <p className="col-span-3 place-self-end bg-gray-200 p-2 text-center rounded-full w-36 text-xs  md:col-span-2 md:place-self-start">
                   Student
                 </p>
-                <p className="col-span-1 bg-green-200 p-2 text-center rounded-full max-w-36 text-xs">
+                <p className="hidden md:block md:col-span-1 bg-green-200 p-2 text-center rounded-full max-w-36 text-xs">
                   Regular
                 </p>
               </div>
